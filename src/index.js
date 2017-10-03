d413971dd9e1dfdfed01dd7344c29a23b7d6e88f
georgy <ggsurkov@gmail.com>
2017-10-03 14:53:14 +0300

    create structure: empty components and pages (expect for NotFound page, this page I think fully complete)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import topics from './data/topics';
+import articles from './data/articles';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import 'material-components-web/dist/material-components-web.css';
+import './index.css';
+
+ReactDOM.render(
+    <Router>
+        <App topics={topics} articles={articles} />
+    </Router>
+    , document.getElementById('root')
+);
